49b912e2321220432cbaca7797e397126bbfc081
Shaun-B1 <shaun.macwilliam@gmail.com>
2020-11-01 23:25:21 +1100

    A skeleton of a game. No victory conditions yet set, but will play 9 turns and fill out the grid

--- Tic Tac Toe.rb
+++ Tic Tac Toe.rb
@@ -1,50 +1,64 @@
 class Player
-  attr_accessor :name, :symbol, :turn
+  attr_accessor :name, :symbol, :turn, :board
 
-  def initialize(name, symbol, turn)
+
+  def initialize(name, symbol, turn, board)
     @name = name
     @symbol = symbol
     @turn = turn
-    #game.players << self
+    @board = board
   end
 
-  #needs to:
-
-    #take player input and assign to an instance variable.
-
-    #turn instance variable
+  def get_input
+    puts "#{self.name}, where do you want to place your mark: "
+    choice = gets.chomp.to_i
+    return choice
+  end
 end
 
 class Board
-  attr_accessor :board
+  attr_accessor :game_board
 
   def initialize
-    @board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    @game_board = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
   def display_board
-    @board.each do |r|
-      puts r.each { |p| p }.join(" ")
-    end
+    puts "#{self.game_board[1]} | #{self.game_board[2]} | #{self.game_board[3]} "
+    puts "---------"
+    puts "#{self.game_board[4]} | #{self.game_board[5]} | #{self.game_board[6]} "
+    puts "---------"
+    puts "#{self.game_board[7]} | #{self.game_board[8]} | #{self.game_board[9]} "
   end
 end
 
 class Game
+  attr_accessor :player_x, :player_o, :board
 
-    #initialize
+  @@turncount = 1
 
-    #new player method
+  def initialize
+    @board = Board.new
+    @player_x = Player.new("Player X", "X", 1, board)
+    @player_o = Player.new("Player O", "O", 2, board)
+  end
 
-    #new board method
-
-    #start a new game that intializes with player x, player o and a new game board.
-
-    #display board
-
-    #count each turn
-
-    #check for a win on each turn
-
-    #after 9 turns, end game
-
+  def play()
+    9.times do
+      if @@turncount.odd?
+        answer = player_x.get_input
+        board.game_board[answer] = "#{player_x.symbol}"
+        board.display_board
+        @@turncount += 1
+      elsif @@turncount.even?
+        answer = player_o.get_input
+        board.game_board[answer] = "#{player_o.symbol}"
+        board.display_board
+        @@turncount += 1
+      end
+    end
+  end
 end
+
+new_game = Game.new
+new_game.play
